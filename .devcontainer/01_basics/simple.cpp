7bb3933dd7b270243f340cf213f26b16ce8a5691
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-02-28 13:39:48 +0000

    testing of program

--- .devcontainer/01_basics/simple.cpp
+++ .devcontainer/01_basics/simple.cpp
@@ -1,1 +1,29 @@
-hiisyfhsig
+#include <iostream>
+using namespace std;
+class Item
+{
+    int Id;
+    float cost;
+
+public:
+    void setData(int a, float b);
+    void getData();
+};
+void Item::setData(int a, float b)
+{
+    Id = a;
+    cost = b;
+}
+void Item::getData()
+{
+    cout << "The Id of the item is : " << Id << endl;
+    cout << "The cost of the item : " << cost << endl;
+}
+int main()
+{
+    Item x, y;
+    x.setData(100, 200);
+    x.getData();
+    y.setData(101, 300);
+    y.getData();
+}
