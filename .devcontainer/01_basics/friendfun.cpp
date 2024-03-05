7cf6accbdd3eea49f49ef2b9c4fe9999b4d71a63
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-03-05 15:56:51 +0000

    Exchange private data

---
+++ .devcontainer/01_basics/friendfun.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class FirstNum;
+class SecondNum{
+       int value1;
+    public:
+       void inData(int a){ value1 = a; } 
+        void sowData(void){
+            cout<<"The value of second num :"<< value1 <<endl;
+        } 
+        friend void Exchange(SecondNum &, FirstNum &);
+};
+class FirstNum{
+    int value2;
+public:
+    void inData(int b){value2 = b;}
+    void sowData(void){
+        cout<<" The value of first Num : "<< value2 <<endl;
+    }
+    friend void Exchange(SecondNum & , FirstNum &);
+};
+ void Exchange(SecondNum & A, FirstNum & B){
+        int temp = A.value1;
+        A.value1 = B.value2;
+        B.value2 = temp;
+ }
+ 
+int main()
+{
+   SecondNum second;
+   FirstNum first;
+   second.inData(100);
+   first.inData(300);
+ cout<<" the value before exchange : "<<endl;
+   second.sowData();
+   first.sowData();
+ cout<<" the value After exchange : "<<endl;
+ Exchange(second,first);
+   second.sowData();
+   first.sowData();
+  return 0;
+}
+
+
