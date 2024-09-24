4fb5f8168160d4e04c87d3135f3581a3acd7a735
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-09-24 12:11:20 +0000

    DP  0\1 knapsack problem using recursion

--- .devcontainer/Recursion/Besic_recursion.cpp
+++ .devcontainer/Recursion/Besic_recursion.cpp
@@ -1,18 +1,47 @@
+// Program to print 1 to n 
+
 #include <iostream>
 using namespace std;
-int print(int n)
+void print(int n)
 {
-    if (n == 0)
-        return 1;
+    if (n == 1)
+    {
+        cout << n << " ";
+        return;
+    }
+
     print(n - 1);
-    std ::cout << n << " ";
+    cout << n << " ";
 }
 
 int main()
 {
     int n;
-    cout<<"Enter the number from that you want to find till"<<endl;
+    cout << "Enter the number from that you want to find till" << endl;
     cin >> n;
     print(n);
     return 0;
 }
+
+// program to print n to 1 
+// #include <iostream>
+// using namespace std;
+// void print(int n)
+// {
+//     if (n == 1)
+//     {
+//         cout << n << " ";
+//         return;
+//     }
+//     cout << n << " ";
+//     print(n - 1);
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number till that you want to find till" << endl;
+//     cin >> n;
+//     print(n);
+//     return 0;
+// }
