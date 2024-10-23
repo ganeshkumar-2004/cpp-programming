b60305f5d457440d770dbf6ce4611b37a7698b62
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-10-23 15:32:20 +0000

    sorting an arrray by using recursion

--- .devcontainer/Recursion/sortingarry.cpp
+++ .devcontainer/Recursion/sortingarry.cpp
@@ -1,25 +1,53 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void sort(vector<int> & v){
-    if(v.size()==0)
+void insert(vector<int> &v, int temp)
+{
+    if (v.size() == 0 || v[v.size() - 1] <= temp)
+    {
+        v.push_back(temp);
         return;
-    else{
-        int m = v[v.size()-1];
+    }
+    else
+    {
+        int val = v[v.size() - 1];
+        v.pop_back();
+        insert(v, temp);
+        v.push_back(val);
+    }
+}
+
+void sort(vector<int> &v)
+{
+    if (v.size() == 0)
+        return;
+    else
+    {
+        int m = v[v.size() - 1];
         v.pop_back();
         sort(v);
-        insert(v,m);
-    } 
-    void insert(vector<int> & v, int temp){
-        if (v.size()==0 || v[v.size()-1]<=temp)     
-        {
-           v.push_back(temp);
-           return;
-        }
-        else {
-            
-        }
-        
+        insert(v, m);
     }
-
+}
+int main()
+{
+    int n;
+    cout << "Enter the value of n: " << " ";
+    cin >> n;
+    vector<int> v;
+    cout << "Enter the value of the vector element: " << " ";
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        cin >> element;
+        v.push_back(element);
+    }
+    sort(v);
+    cout<<"The sorted array : "<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<v[i];
+        cout<<" ";
+    }
+    
 }
