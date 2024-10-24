0c1608f53503d8683fe9da29dcfc4023d46da8aa
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-10-24 16:01:58 +0000

    Sortng a stack using Recursion

---
+++ .devcontainer/Recursion/sorting_stack.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void insert(stack<int> &s, int temp){
+    if(s.size()==0 || s.top()<=temp)
+        s.push(temp);
+    else{
+        int val = s.top();
+        s.pop(); 
+        insert(s,temp);
+        s.push(val);
+    }
+}
+void sort(stack<int> &s)
+{
+    if (s.size() == 0)
+    {
+        return;
+    }
+    else
+    {
+        int temp = s.top();
+        s.pop(); 
+        sort(s);
+        insert(s,temp);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the size of stack: "<<" ";
+    cin>>n;
+    stack<int> s;
+    cout<<"Enter the element of stack: "<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        cin>>element;
+        s.push(element);
+
+    }
+    sort(s);
+    cout<<"Sorted stack is : "<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<s.top()<<" ";
+        s.pop();
+
+    }
+    
+    
+}
