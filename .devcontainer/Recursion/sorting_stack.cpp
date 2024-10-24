611ad022b9eeb6918bae43eb6ce46b21b5e7a452
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-10-24 16:05:15 +0000

    Sorting of a stack by using recursion

--- .devcontainer/Recursion/sorting_stack.cpp
+++ .devcontainer/Recursion/sorting_stack.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-void insert(stack<int> &s, int temp){
-    if(s.size()==0 || s.top()<=temp)
+void insert(stack<int> &s, int temp)
+{
+    if (s.size() == 0 || s.top() <= temp)
         s.push(temp);
-    else{
+    else
+    {
         int val = s.top();
-        s.pop(); 
-        insert(s,temp);
+        s.pop();
+        insert(s, temp);
         s.push(val);
     }
 }
@@ -20,32 +22,31 @@ void sort(stack<int> &s)
     else
     {
         int temp = s.top();
-        s.pop(); 
+        s.pop();
         sort(s);
-        insert(s,temp);
+        insert(s, temp);
     }
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the size of stack: "<<" ";
-    cin>>n;
+    cout << "Enter the size of stack: " << " ";
+    cin >> n;
     stack<int> s;
-    cout<<"Enter the element of stack: "<<" ";
+    cout << "Enter the element of stack: " << " ";
     for (int i = 0; i < n; i++)
     {
         int element;
-        cin>>element;
+        cin >> element;
         s.push(element);
-
     }
     sort(s);
-    cout<<" Your Sorted stack is : "<<" ";
+    cout << " Your Sorted stack is : " << " ";
     for (int i = 0; i < n; i++)
     {
-        cout<<s.top()<<" ";
+        cout << s.top() << " ";
         s.pop();
-
     }
-    
-    
+
+    return 0;
 }
