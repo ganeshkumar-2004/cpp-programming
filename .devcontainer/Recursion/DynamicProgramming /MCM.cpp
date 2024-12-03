2c5d60ffad2546ee55fe12df9b726adaa7d4e7f3
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-12-03 15:32:23 +0000

    matrix chain multiplication problem

--- .devcontainer/Recursion/DynamicProgramming /MCM.cpp
+++ .devcontainer/Recursion/DynamicProgramming /MCM.cpp
@@ -2,39 +2,40 @@
 #include <climits>
 #include <cstring>
 using namespace std;
-int static t[10001][1001];
+int static t[100][100];
 int solve(int arr[], int i, int j)
 {
-    int mn = INT_MAX;
+    if (i == j)
+        return 0;
+
     if (t[i][j] != -1)
     {
         return t[i][j];
     }
-    else
+    int mn = INT_MAX;
+
+    for (int k = i; k <= (j - 1); k++)
     {
-        for (int k = i; k <= (j-1); k++)
-        {
-            int tempAns = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
-            if (tempAns < mn)
-            {
-                mn = tempAns;
-                t[i][j] = mn;
-            }
-        }
+        int tempAns = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+        mn = min(mn, tempAns);
+        t[i][j] = mn;
     }
+
     return t[i][j];
 }
 int main()
 {
     memset(t, -1, sizeof(t));
+    cout << "Enter the size of array which containing the dimentaion of matrices :  ";
     int n;
     cin >> n;
     int arr[n];
+    cout << "Enter the dimentaion of matrices :  ";
 
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    cout<<solve(arr,1,n-1);
+    cout << "Minimum cost of matrix chain multiplication : " << solve(arr, 1, n - 1) << endl;
     return 0;
 }
