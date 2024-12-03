cf26f8472b46eefb1853ae6433bccb9ed2912208
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-12-03 14:54:59 +0000

    String permutatin problem

---
+++ .devcontainer/Recursion/Backtracking/StrPermutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class StrPermutation
+{
+public:
+    void Permute(string ip, string op, vector<string> &v)
+    {
+        if (ip.size() == 0)
+        {
+            v.push_back(op);
+            return;
+        }
+        unordered_set<char> mp;
+        for (int i = 0; i < ip.size(); i++)
+        {
+            if (mp.find(ip[i]) == mp.end())
+            {
+                mp.insert(ip[i]);
+            }
+            string newIP = ip.substr(0, i) + ip.substr(i + 1);
+            string newOP = op + ip[i];
+            Permute(newIP, newOP, v);
+        }
+    }
+};
+int main(){
+    string ip="ABC";
+    vector<string> Result;
+    StrPermutation Sp;
+    Sp.Permute(ip,"",Result);
+    cout << "Unique permutations are: " << endl;
+    for (const string& perm : Result) {
+        cout << perm << endl;
+    }
+
+    return 0;
+}
